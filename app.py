62c86c4440c5a1f17ba9869d4af91a7f0a5f3678
RiddSarav <68668476+RiddSarav@users.noreply.github.com>
2022-12-30 12:25:36 -0800

    Update app.py

--- app.py
+++ app.py
@@ -20,7 +20,7 @@ def home():
 @app.route("/about")
 def about():
     return render_template("about.html")
-@app.route('/contact'):
+@app.route('/contact')
 def contact():
     return render_template("contact.html")
 if __name__ == '__main__':
